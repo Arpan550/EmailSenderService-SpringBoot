9ce99572091bd8eb3b2e9b6547182f4b6c9bb087
Arpan550 <arpanbhattacharya5356@gmail.com>
2025-04-10 15:07:00 +0530

    Added an frontend origin

--- src/main/java/com/example/medichain/medichain/controller/PasswordResetController.java
+++ src/main/java/com/example/medichain/medichain/controller/PasswordResetController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "https://medichain.pages.dev"})// Allow your frontend origin
+@CrossOrigin("https://medichain.pages.dev")// Allow your frontend origin
 public class PasswordResetController {
 
     @Autowired
