fb7205de5d038103c3940baf4fd53fc1cfdddd85
Arpan550 <arpanbhattacharya5356@gmail.com>
2025-04-10 14:19:46 +0530

    Removed external footer-links

--- src/main/java/com/example/medichain/medichain/service/EmailSenderService.java
+++ src/main/java/com/example/medichain/medichain/service/EmailSenderService.java
@@ -70,9 +70,9 @@ public class EmailSenderService {
                     "  <div class='email-footer'>" +
                     "  <div class='footer-logo' style='color: #000;'>MediChain</div>" +
                     "    <div class='footer-links'>" +
-                    "      <a href='https://medichain.pages.dev/privacy-policy'>Privacy Policy</a> | " +
-                    "      <a href='https://medichain.pages.dev/terms-of-service'>Terms of Service</a> | " +
-                    "      <a href='https://medichain.pages.dev/help-center'>Help Center</a>" +
+                    "      <a href='#'>Privacy Policy</a> | " +
+                    "      <a href='#'>Terms of Service</a> | " +
+                    "      <a href='#'>Help Center</a>" +
                     "    </div>" +
                     "    <div class='social-links'>" +
                     "    </div>" +
